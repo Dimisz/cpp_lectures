0f15b177cdca6738e53c3a0f97e038d3de4bfdc0
Vladimir Solovyov <leilyngale@icloud.com>
2021-11-18 10:27:10 +0800

    week2: Second lecture on maps

---
+++ moscow_institute_of_physics_and_technology_course/course1/maps_lecture2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+void PrintMap(const map<string, int>& m){
+  for (const auto& item : m){ //USEFUL TRICK
+    cout << item.first << ": " << item.second << endl;
+  }
+}
+
+
+map<string, int> MakeCountMap(const vector<string>& v){
+  map<string, int> result;
+  for (const auto& i : v){
+// VERBOSE ==================
+//    // if (result.count(i) == 0)  // INSTRUCTORS TAKE
+//    if (result.find(i) == result.end()){
+//      result[i] = 1;
+//    }
+//    else {
+//      result[i] += 1;
+//    }
+// END VERBOSE  ====
+    ++result[i]; // if not there the elt will be created
+    // if there - will be incremented
+  }
+  return result;
+}
+
+
+
+int main(){
+  map<string, int> m = {{"one", 1}, {"two", 2}, {"three", 3}};
+  PrintMap(m);
+  
+  // map.erase(key) -> takes only one arg
+  m.erase("three");
+  PrintMap(m);
+  
+  // Counting words in vector using map
+  cout << "========COUNTING WORDS IN VEC==========" << endl;
+  vector<string> some_words = {"one", "two", "one", "five" , "three", "two"};
+  map <string, int> words_occurences = MakeCountMap(some_words);
+  
+  PrintMap(words_occurences);
+  
+  cout << "=========QUIZ==============" << endl;
+  vector<string> words = {"one", "two", "three"};
+  map<char, vector<string>> grouped_words;
+  for (const string& word : words) {
+    grouped_words[word[0]].push_back(word);
+  }
+  for (const auto& item : grouped_words) {
+    cout << item.first << endl;
+    for (const string& word : item.second) {
+      cout << word << " ";
+    }
+    cout << endl;
+  }
+  
+  
+  return 0;
+}
