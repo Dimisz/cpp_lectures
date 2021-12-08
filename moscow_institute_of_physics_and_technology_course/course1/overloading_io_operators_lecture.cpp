f612339127ebbe78589a6a8dceb346e212f925bb
Vladimir Solovyov <leilyngale@icloud.com>
2021-12-08 20:18:03 +0800

    Lecture on operators overlloading

---
+++ moscow_institute_of_physics_and_technology_course/course1/overloading_io_operators_lecture.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+struct Duration {
+  int hour;
+  int min;
+};
+
+Duration ReadDuration(istream& stream){
+  int h = 0;
+  int m = 0;
+  stream >> h;
+  stream.ignore(1);
+  stream >> m;
+  return Duration {h, m};
+}
+
+void PrintDuration(ostream& stream, const Duration& duration){
+  stream << setfill('0');
+  stream << setw(2) << duration.hour << ':'
+  << setw(2) << duration.min;
+}
+
+void operator<<(ostream& stream, const Duration& duration){
+  
+}
+int main(){
+  stringstream dur_ss("01:50");
+  Duration dur1 = ReadDuration(dur_ss);
+  PrintDuration(cout, dur1);
+  
+  return 0;
+}
