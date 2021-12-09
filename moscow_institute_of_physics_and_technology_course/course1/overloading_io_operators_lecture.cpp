3738f6d8889dbb0257206f75f694f95916b250ad
Vladimir Solovyov <leilyngale@icloud.com>
2021-12-10 07:44:26 +0800

    Great lecture on overloading operators

--- moscow_institute_of_physics_and_technology_course/course1/overloading_io_operators_lecture.cpp
+++ moscow_institute_of_physics_and_technology_course/course1/overloading_io_operators_lecture.cpp
@@ -9,30 +9,76 @@ using namespace std;
 struct Duration {
   int hour;
   int min;
+  
+  Duration(int h = 0, int m = 0){
+    int total = h * 60 + m;
+    hour = total / 60;
+    min = total % 60;
+  }
 };
 
-Duration ReadDuration(istream& stream){
-  int h = 0;
-  int m = 0;
-  stream >> h;
-  stream.ignore(1);
-  stream >> m;
-  return Duration {h, m};
-}
+//Duration ReadDuration(istream& stream){
+//  int h = 0;
+//  int m = 0;
+//  stream >> h;
+//  stream.ignore(1);
+//  stream >> m;
+//  return Duration {h, m};
+//}
 
-void PrintDuration(ostream& stream, const Duration& duration){
+//void PrintDuration(ostream& stream, const Duration& duration){
+//  stream << setfill('0');
+//  stream << setw(2) << duration.hour << ':'
+//  << setw(2) << duration.min;
+//}
+
+//OVERLOADING << DOES THE SAME AS THE FUNC ABOVE
+ostream& operator<<(ostream& stream, const Duration& duration){
   stream << setfill('0');
   stream << setw(2) << duration.hour << ':'
   << setw(2) << duration.min;
+  return stream;
 }
 
-void operator<<(ostream& stream, const Duration& duration){
-  
+istream& operator>>(istream& stream, Duration& duration){
+    stream >> duration.hour;
+    stream.ignore(1);
+    stream >> duration.min;
+    return stream;
+}
+
+bool operator<(const Duration& lhs, const Duration& rhs){
+  if(lhs.hour == rhs.hour){
+    return lhs.min < rhs.min;
+  }
+  return lhs.hour < rhs.hour;
+}
+
+Duration operator+(const Duration& lhs, const Duration& rhs){
+  return Duration(lhs.hour + rhs.hour, lhs.min + rhs.min);
+}
+
+void PrintVector(const vector<Duration>& durs){
+  for (const auto& d : durs){
+    cout << d << ' ';
+  }
+  cout << endl;
 }
 int main(){
-  stringstream dur_ss("01:50");
-  Duration dur1 = ReadDuration(dur_ss);
-  PrintDuration(cout, dur1);
+  stringstream dur_ss("3:50");
+  //Duration dur1 = ReadDuration(dur_ss);
+  //PrintDuration(cout, dur1);
   
+  Duration dur1;
+  dur_ss >> dur1;
+  Duration dur2 = {0, 35};
+  Duration dur3 = dur1 + dur2;
+  vector<Duration> v{
+    dur3, dur1, dur2
+  };
+  PrintVector(v);
+  sort(begin(v), end(v));
+  PrintVector(v);
+  //cout << dur1 + dur2 << endl;
   return 0;
 }
