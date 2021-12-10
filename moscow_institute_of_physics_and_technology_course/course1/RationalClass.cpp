1b772383d8a02469854096f7ca9139923fd54776
Vladimir Solovyov <leilyngale@icloud.com>
2021-12-10 11:10:59 +0800

    Initial solution of 4/5 problems

--- moscow_institute_of_physics_and_technology_course/course1/RationalClass.cpp
+++ moscow_institute_of_physics_and_technology_course/course1/RationalClass.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <numeric>
+#include <algorithm>
+#include <sstream>
+#include <iomanip>
+#include <set>
+#include <map>
 
 using namespace std;
 
@@ -17,7 +22,11 @@ public:
   };
   
   Rational(int n, int d){
-    if ((n < 0) && (d >= 0)){
+    if (n == 0){
+      numerator = 0;
+      denominator = 1;
+    }
+    else if ((n < 0) && (d >= 0)){
       numerator = n / gcd(-n, d);
       denominator = d / gcd(-n, d);
     }
@@ -28,10 +37,6 @@ public:
     else if ((n < 0) && (d < 0)){
       numerator = -n / gcd(-n, -d);
       denominator = -d / gcd(-n, -d);
-    }
-    else if (n == 0){
-      numerator = 0;
-      denominator = 1;
     }
     else {
       numerator = n / gcd(n, d);
@@ -73,6 +78,35 @@ Rational operator-(const Rational& lhs, const Rational& rhs){
   return Rational(num, den);
 }
 
+Rational operator*(const Rational& lhs, const Rational& rhs){
+  int num = (lhs.Numerator() * rhs.Numerator());
+  int den = (lhs.Denominator() * rhs.Denominator());
+  return Rational(num, den);
+}
+
+Rational operator/(const Rational& lhs, const Rational& rhs){
+  int num = (lhs.Numerator() * rhs.Denominator());
+  int den = (lhs.Denominator() * rhs.Numerator());
+  return Rational(num, den);
+}
+
+
+
+ostream& operator<<(ostream& stream, const Rational& r){
+  stream << r.Numerator() << "/" << r.Denominator();
+  return stream;
+}
+
+istream& operator>>(istream& stream, Rational& r){
+  int num, den;
+  stream >> num;
+  stream.ignore(1);
+  stream >> den;
+  r = Rational(num, den);
+  
+  return stream;
+}
+
 
 
 int main(){
@@ -83,12 +117,28 @@ int main(){
 //  cout << r1.Numerator() << '/' << r1.Denominator();
 //  cout << endl;
   //cout << gcd(5, 6) << endl;
-  cout << "OK" << endl;
-  cout << "====================" << endl;
-  const Rational r = Rational(1, 2) + Rational(1, 3) - Rational(1, 4);
-  if (r == Rational(7, 12)) {
-    cout << "equal";
-  }
+//  cout << "OK" << endl;
+//  cout << "====================" << endl;
+//  const Rational r = Rational(1, 2) + Rational(1, 3) - Rational(1, 4);
+//  if (r == Rational(7, 12)) {
+//    cout << "equal";
+//  }
   
+//  const Rational r = Rational(1, 2) * Rational(1, 3) / Rational(1, 4);
+//  if (r == Rational(2, 3)) {
+//    cout << "equal";
+//  }
+//  Rational r;
+//  cin >> r;
+//  cout << r;
+  set<Rational> rationals;
+  rationals.insert(Rational(1, 2));
+  rationals.insert(Rational(1, 3));
+//
+//  map<Rational, string> name;
+//  name[Rational(1, 2)] = "одна вторая";
+//  cout << Rational(1, 2);
+//  cout << Rational(1, 3);
+//  
   return 0;
 }
