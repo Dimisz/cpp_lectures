78d8297773d8410ab88fd6830d52a482902ae21b
Vladimir Solovyov <leilyngale@icloud.com>
2021-12-16 07:50:25 +0800

    More on default args

--- frank_mitropoulos_course/functions2.cpp
+++ frank_mitropoulos_course/functions2.cpp
@@ -5,8 +5,14 @@ using namespace std;
 
 double calc_cost(double base_cost, double tax_rate = 0.06, double shipping = 3.5);
 
+void greeting(string name, string prefix = "Mr.", string suffix = "");
+
 double calc_cost(double base_cost, double tax_rate, double shipping){
   return base_cost += (base_cost * tax_rate) + shipping;
+}
+
+void greeting(string name, string prefix, string suffix){
+  cout << "Hello " << prefix + " " + name + " " + suffix << endl;
 }
 int main(){
   double cost{0};
@@ -21,6 +27,9 @@ int main(){
   cost = calc_cost(200.0);
   cout << "Cost is: " << cost << endl;
   
-  
+  greeting("Glenn Jones", "Dr", "M.D.");
+  greeting("James Roger", "Professor", "Ph.D");
+  greeting("Frank Miller", "Dr");
+  greeting("William Smmith");
   return 0;
 }
