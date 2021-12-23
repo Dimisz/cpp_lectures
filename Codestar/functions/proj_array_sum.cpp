665d28155d1422534f2b7c3f9836c33bc11e57c7
Vladimir Solovyov <leilyngale@icloud.com>
2021-12-23 09:32:01 +0800

    Passing arg by reference

--- Codestar/functions/proj_array_sum.cpp
+++ Codestar/functions/proj_array_sum.cpp
@@ -5,17 +5,27 @@ using namespace std;
 
 int SumArray(int arr[], int arr_size);
 int SumArrayObj(array<int, 8> arr);
+void SumArrayObj(array<int, 8> arr, int& sum);
 
 int main()
 {
+  //C-style array
   int arr[] { 2, 5, 2, 10, 20, 15, 100, 25 };
   int size = 8;
   
-  cout << SumArray(arr, size) << endl;
+  cout << "Sum of C-style array:\t"
+       << SumArray(arr, size) << endl;
   
+  //Array Object
   array<int, 8> array_obj { 2, 5, 2, 10, 20, 15, 100, 25 };
-  cout << SumArrayObj(array_obj) << endl;
+  cout << "Sum of Array Object:\t"
+       << SumArrayObj(array_obj) << endl;
   
+  //Array Object and sum var passed by reference
+  int array_sum {0};
+  SumArrayObj(array_obj, array_sum);
+  cout << "Sum passed by reference:\t"
+       << array_sum << endl;
   return 0;
 }
 
@@ -37,4 +47,12 @@ int SumArrayObj(array<int, 8> arr)
     sum += arr[i];
   }
   return sum;
+}
+
+void SumArrayObj(array<int, 8> arr, int& sum)
+{
+  for (int i = 0; i < arr.size(); i++)
+  {
+    sum += arr[i];
+  }
 }
