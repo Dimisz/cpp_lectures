436b1934dd7d714b5ad93537897a204f5451d32d
Vladimir Solovyov <leilyngale@icloud.com>
2021-12-23 09:09:45 +0800

    new tests

--- Codestar/functions/proj_array_sum.cpp
+++ Codestar/functions/proj_array_sum.cpp
@@ -7,8 +7,8 @@ int SumArray(int arr[], int arr_size);
 
 int main()
 {
-  int arr[] = {1, 2, 3, 4, 5};
-  int size = 5;
+  int arr[] { 2, 5, 2, 10, 20, 15, 100, 25 };
+  int size = 8;
   
   cout << SumArray(arr, size) << endl;
   
