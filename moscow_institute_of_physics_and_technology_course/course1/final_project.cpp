bb44787bce86a9ae9d155083e818ae8eaee28b43
Vladimir Solovyov <leilyngale@icloud.com>
2021-12-27 11:33:48 +0800

    Accepted

--- moscow_institute_of_physics_and_technology_course/course1/final_project.cpp
+++ moscow_institute_of_physics_and_technology_course/course1/final_project.cpp
@@ -12,6 +12,15 @@ class Date {
 public:
   Date(int year, int month, int day)
   {
+    if (month < 1 || month > 12)
+    {
+      throw out_of_range("Month value is invalid: " + to_string(month));
+    }
+    else if (day < 1 || day > 31)
+    {
+      throw out_of_range("Day value is invalid: " + to_string(day));
+
+    }
     this->year = year;
     this->month = month;
     this->day = day;
@@ -37,27 +46,27 @@ private:
 };
 
 
-bool PrepareDate(int year, int month, int day)
-{
-  bool date_legit = false;
-  
-  if (month > 0 && month <= 12)
-  {
-    if (day > 0 && day <= 31)
-    {
-      date_legit = true;
-    }
-    else
-    {
-      cout << "Day value is invalid: " << day << endl;
-    }
-  }
-  else
-  {
-    cout << "Month value is invalid: " << month << endl;
-  }
-  return date_legit;
-}
+//bool PrepareDate(int year, int month, int day)
+//{
+//  bool date_legit = false;
+//
+//  if (month > 0 && month <= 12)
+//  {
+//    if (day > 0 && day <= 31)
+//    {
+//      date_legit = true;
+//    }
+//    else
+//    {
+//      cout << "Day value is invalid: " << day << endl;
+//    }
+//  }
+//  else
+//  {
+//    cout << "Month value is invalid: " << month << endl;
+//  }
+//  return date_legit;
+//}
 
 
 bool operator<(const Date& lhs, const Date& rhs)
@@ -80,30 +89,30 @@ public:
     db[date].insert(event);
   }
   //bool changed to void
-  void DeleteEvent(const Date& date, const string& event)
+  bool DeleteEvent(const Date& date, const string& event)
   {
     if (event.empty())
     {
       cout << "Deleted " << DeleteDate(date) << " events" << endl;
-      //return true;
+      return true;
     }
     else
     {
       if(db[date].erase(event))
       {
         cout << "Deleted successfully" << endl;
-        //return true;
+        return true;
       }
       else{
         cout << "Event not found" << endl;
-        //return false;
+        return false;
       }
     }
   }
   
   int  DeleteDate(const Date& date)
   {
-    int num_events = db[date].size();
+    int num_events = db.at(date).size();
     db.erase(date);
     return num_events;
   }
@@ -149,90 +158,99 @@ private:
 };
 
 int main() {
-  Database db;
-    
-  string command;
-  while (getline(cin, command)) {
-    
-   
-    istringstream iss(command);
-    string operation;
-    if(iss >> operation)
-    {
-      //cout << "Operation: " << operation << endl;
-      if (operation == "Print")
-      {
-        db.Print();
-      }
-      else
-      {
-        string date;
-        iss >> date;
-        //VALIDATING DATE
-        //IF VALIDATED -> PROCEED
-        istringstream date_stream(date);
-        int year, month, day;
-        if (date_stream >> year)
+  try
+  {
+  
+      Database db;
+        
+      string command;
+      while (getline(cin, command)) {
+        
+       
+        istringstream iss(command);
+        string operation;
+        if(iss >> operation)
         {
-          date_stream.ignore(1);
-          if(date_stream >> month)
+          //cout << "Operation: " << operation << endl;
+          if (operation == "Print")
           {
-            date_stream.ignore(1);
-            if((date_stream >> day))
+            db.Print();
+          }
+          else
+          {
+            string date;
+            iss >> date;
+            //VALIDATING DATE
+            //IF VALIDATED -> PROCEED
+            istringstream date_stream(date);
+            int year, month, day;
+            if (date_stream >> year)
             {
-              //Date validated
-              //Procceed to operations
-              //cout << boolalpha << PrepareDate(year, month, day) << endl;
-              string event;
-              iss >> event;
-              if (operation == "Add")
+              date_stream.ignore(1);
+              if(date_stream >> month)
               {
-                if(PrepareDate(year, month, day))
+                date_stream.ignore(1);
+                if((date_stream >> day))
                 {
-                  Date d(year, month, day);
-                  db.AddEvent(d, event);
+                  //Date validated
+                  //Procceed to operations
+                  //cout << boolalpha << PrepareDate(year, month, day) << endl;
+                  string event;
+                  iss >> event;
+                  if (operation == "Add")
+                  {
+    //                if(PrepareDate(year, month, day))
+    //                {
+                      Date d(year, month, day);
+                      db.AddEvent(d, event);
+    //                }
+                  }
+                  else if (operation == "Del")
+                  {
+    //                if(PrepareDate(year, month, day))
+    //                {
+                      Date d(year, month, day);
+                      db.DeleteEvent(d, event);
+    //                }
+                  }
+                  else if (operation == "Find")
+                  {
+    //                if(PrepareDate(year, month, day))
+    //                {
+                      Date d(year, month, day);
+                      db.Find(d);
+    //                }
+                  }
+                  else
+                  {
+                    throw logic_error("Unknown command: " + operation);
+                  }
                 }
-              }
-              else if (operation == "Del")
-              {
-                if(PrepareDate(year, month, day))
+                else
                 {
-                  Date d(year, month, day);
-                  db.DeleteEvent(d, event);
-                }
-              }
-              else if (operation == "Find")
-              {
-                if(PrepareDate(year, month, day))
-                {
-                  Date d(year, month, day);
-                  db.Find(d);
+                  throw logic_error("Wrong date format: " + date);
                 }
               }
               else
               {
-                cout << "Unknown command: " << operation << endl;
+                throw logic_error("Wrong date format: " + date);
               }
             }
             else
             {
-              cout << "Wrong date format: " << date << endl;
-            }
-          }
-          else
-          {
-            cout << "Wrong date format: " << date << endl;
+              throw logic_error("Wrong date format: " + date);
+            }//end of date validation
           }
         }
-        else
-        {
-          cout << "Wrong date format: " << date << endl;
-        }//end of date validation
+
+
       }
-    }
 
-
+      return 0;
   }
-
-  return 0;
+  catch(exception& e)
+  {
+    cout << e.what() << endl;
+    return 0;
+  }
 }
