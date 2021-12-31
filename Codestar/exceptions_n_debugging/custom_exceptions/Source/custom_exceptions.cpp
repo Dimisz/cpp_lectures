4b39a74caf4423439921edb38dd28f79de219746
Vladimir Solovyov <leilyngale@icloud.com>
2021-12-31 17:09:50 +0800

    Creating custom exceptions

---
+++ Codestar/exceptions_n_debugging/custom_exceptions/Source/custom_exceptions.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "AngryCatException.h"
+using namespace std;
+
+void FeedKitty(int num_treats);
+
+int main()
+{
+  
+  int num_treats = 0;
+  cout << "How many treats do you want to feed kitty?" << endl;
+  cin >> num_treats;
+  
+  try
+  {
+    FeedKitty(num_treats);
+  }
+  catch(const AngryCatException& err)
+  {
+    cout << err.what() << endl;
+  }
+  return 0;
+}
+
+void FeedKitty(int num_treats)
+{
+  if(num_treats < 3)
+  {
+    throw AngryCatException();
+  }
+  cout << "Kitty is happy with " << num_treats << " treats"
+         << endl;
+}
